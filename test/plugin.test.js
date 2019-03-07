0d0a84e2c3a18adad4509e8e23593ce5dffa5ed3
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2019-03-07 20:06:25 +0330

    Add more tests

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -30,6 +30,14 @@ describe('Parse Valid String', () => {
     expect(date.date()).toEqual(13)
   })
 
+  const date2 = dayjs('1397/06', { jalali: true })
+
+  test('valid jalali date without day', () => {
+    expect(date2.year()).toEqual(1397)
+    expect(date2.month()).toEqual(5)
+    expect(date2.date()).toEqual(1)
+  })
+
   const gregory = date.calendar('gregory')
 
   test('convert to gregory', () => {
@@ -55,12 +63,60 @@ it('startOfMonth', () => {
   expect(date2.date()).toEqual(1)
 })
 
+it('endOfMonth - months with 31 days', () => {
+  const date = dayjs('1397/06/13', { jalali: true })
+  const date2 = date.endOf('month')
+  expect(date2.year()).toEqual(date.year())
+  expect(date2.month()).toEqual(date.month())
+  expect(date2.date()).toEqual(31)
+})
+
+it('endOfMonth - months with 30 days', () => {
+  const date = dayjs('1397/07/13', { jalali: true })
+  const date2 = date.endOf('month')
+  expect(date2.year()).toEqual(date.year())
+  expect(date2.month()).toEqual(date.month())
+  expect(date2.date()).toEqual(30)
+})
+
+it('endOfMonth - months with 29 days', () => {
+  const date = dayjs('1397/12/13', { jalali: true })
+  const date2 = date.endOf('month')
+  expect(date2.year()).toEqual(date.year())
+  expect(date2.month()).toEqual(date.month())
+  expect(date2.date()).toEqual(29)
+})
+
+it('endOfMonth - months with 29 days - leap year', () => {
+  const date = dayjs('1399/12/13', { jalali: true })
+  const date2 = date.endOf('month')
+  expect(date2.year()).toEqual(date.year())
+  expect(date2.month()).toEqual(date.month())
+  expect(date2.date()).toEqual(30)
+})
+
 it('startOfYear', () => {
   const date = dayjs('1397/06/13', { jalali: true })
   const date2 = date.startOf('year')
   expect(date2.year()).toEqual(date.year())
   expect(date2.month()).toEqual(0)
   expect(date2.date()).toEqual(1)
+})
+
+it('endOfYear', () => {
+  const date = dayjs('1397/06/13', { jalali: true })
+  const date2 = date.endOf('year')
+  expect(date2.year()).toEqual(date.year())
+  expect(date2.month()).toEqual(11)
+  expect(date2.date()).toEqual(29)
+})
+
+it('endOfYear - leap year', () => {
+  const date = dayjs('1399/06/13', { jalali: true })
+  const date2 = date.endOf('year')
+  expect(date2.year()).toEqual(date.year())
+  expect(date2.month()).toEqual(11)
+  expect(date2.date()).toEqual(30)
 })
 
 it('startOfWeek', () => {
@@ -75,10 +131,31 @@ it('endOfWeek', () => {
   expect(date2.day()).toEqual(5)
 })
 
+it('dayInMonth, months with 31 days', () => {
+  expect(dayjs('1397/06/13', { jalali: true }).daysInMonth()).toEqual(31)
+  expect(dayjs('1396/05/13', { jalali: true }).daysInMonth()).toEqual(31)
+  expect(dayjs('1395/04/13', { jalali: true }).daysInMonth()).toEqual(31)
+  expect(dayjs('1393/03/13', { jalali: true }).daysInMonth()).toEqual(31)
+  expect(dayjs('1392/02/13', { jalali: true }).daysInMonth()).toEqual(31)
+  expect(dayjs('1391/01/13', { jalali: true }).daysInMonth()).toEqual(31)
+})
 
-it('dayInMonth', () => {
-  const date = dayjs('1397/06/13', { jalali: true })
-  expect(date.daysInMonth()).toEqual(31)
+it('dayInMonth, months with 30 days', () => {
+  expect(dayjs('1397/11/13', { jalali: true }).daysInMonth()).toEqual(30)
+  expect(dayjs('1396/10/13', { jalali: true }).daysInMonth()).toEqual(30)
+  expect(dayjs('1395/09/13', { jalali: true }).daysInMonth()).toEqual(30)
+  expect(dayjs('1394/08/13', { jalali: true }).daysInMonth()).toEqual(30)
+  expect(dayjs('1393/07/13', { jalali: true }).daysInMonth()).toEqual(30)
+})
+
+it('dayInMonth - months with 29 days in leap years', () => {
+  const date = dayjs('1399/12/13', { jalali: true })
+  expect(date.daysInMonth()).toEqual(30)
+})
+
+it('dayInMonth - months with 29 days', () => {
+  const date = dayjs('1397/12/13', { jalali: true })
+  expect(date.daysInMonth()).toEqual(29)
 })
 
 it('format', () => {
@@ -134,4 +211,3 @@ describe('diff two date', () => {
     expect(a.diff(b, 'year')).toEqual(0)
   })
 })
-
